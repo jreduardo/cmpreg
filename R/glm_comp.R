408353e0c9e8bf8dade046c6b562fe14fe20e502
Eduardo Junior <edujrrib@gmail.com>
2016-03-20 16:40:03 -0300

    Adiciona funções para estimação dos parâmetros do modelo COM-Poisson usual

---
+++ R/glm_comp.R
@@ -0,0 +1,66 @@
+#' @title Log-Verossimilhança do Modelo Conway-Maxwell-Poisson
+#' @description Calcula a log-verossimilhança de um modelo COM-Poisson
+#'     considerando os dados e as estimativas dos parâmetros informadas.
+#' @details A função de log-verossimilhança toma a forma: \deqn{-Z - y *
+#'     \lambda - \nu \log{y!}}, onde \eqn{Z = \sum
+#'     \frac{\lambda^i}{(i!)^\nu}}
+#' @param betas Um vetor de estimativas para os parâmetros de regressão
+#'     da distribuição Conway-Maxwell-Poisson.
+#' @param phi Um valor estimado para o parâmetro de dispersão considerado
+#'     na distrbuição Conway-Maxwell-Poisson \eqn{\phi = \log{\nu}}
+#' @param y Um vetor de contagens, considerado como variável resposta
+#' @param X A matriz de delineamento do modelo
+#' @return O valor da log-verossimilhança do modelo
+#'     Conway-Maxwell-Poisson com os parâmetros e dados informados
+#' @author Eduardo E. R. Junior, \email{edujrrib@gmail.com}
+#' @seealso \code{\link[tccPackage]{glm_cmp}}
+
+ll_cmp <- function(betas, phi, y, X){
+    nu <- exp(phi)
+    Xb <- X %*% betas
+    kernel <- sum(y * Xb - nu * lfactorial(y))
+    ## Obtendo a constante normatizadora Z.
+    ## WARNING: Verificar a qtde de termos para a soma infinita
+    i <- 1:150
+    zs <- sapply(Xb, function(lam) sum(exp(i * lam - nu * lfactorial(i))))
+    Z <- sum(log(zs + 1))
+    ll <- kernel - Z
+    return(ll)
+}
+
+#' @title Estimação do Modelo Conway-Maxwell-Poisson
+#' @description Estima os parâmetros de um modelo COM-Poisson sob a
+#'     otimização da função de log-verossimilhança. A sintaxe
+#'     assemelha-se com a função \code{\link{glm}} (Generalized Linear
+#'     Models).
+#' @param formula Um objeto da classe \code{\link{formula}}
+#' @param data Um objeto de classe \code{data.frame}, cujo contém as
+#'     variáveis descritas na \code{formula}
+#' @param subset Um vetor lógico opcional, indicando um sub-grupo do
+#'     \code{data.frame} especificado em \code{data}
+#' @param ... Argumentos opcionais do framework de maximização numérica
+#'     \code{\link{optim}}
+#' @return Uma lista com de valores retornados da \code{\link{optim}}
+#' @author Eduardo E. R. Junior, \email{edujrrib@gmail.com}
+#' @seealso \code{\link[tccPackage]{ll_cmp}}
+#' @export
+
+glm_cmp <- function(formula, data, subset = NULL, ...) {
+    frame <- model.frame(formula, data, subset = subset)
+    terms <- attr(frame, "terms")
+    ##
+    y <- model.response(frame)
+    X <- model.matrix(terms, frame)
+    ##
+    poissonModel <- glm(formula, data = data, subset = subset,
+                        family = poisson)
+    betas <- poissonModel$coefficients
+    phi <- 0
+    ##
+    fn <- function(params) {
+        ll_cmp(params[-1], params[1], y = y, X = X)
+    }
+    opt <- optim(c(phi, betas), fn = fn, method = "BFGS",
+                 hessian = TRUE, ...)
+    return(opt)
+}
