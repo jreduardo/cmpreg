f73e11730976463fa3e92281c8de36b4c9bdbdbf
Eduardo Junior <edujrrib@gmail.com>
2016-04-23 22:00:43 -0300

    Adiciona função para cálculo da constante Z
    A função é equivalente ao cálculo anteriormente usado, conforme visto
    na seção examples, porém quando incorpora à funcão de verossimilhança
    leva o otimizar a propor valores absurdos para phi. Necessita de
    investigação.

--- R/cmp.R
+++ R/cmp.R
@@ -1,3 +1,76 @@
+#' @title Avaliação da Constante Normalizadora
+#' @description Calcula o valor da constante de normalização do modelo
+#'     COM-Poisson definida por: \deqn{Z = \sum
+#'     \frac{\lambda^i}{(i!)^\nu}}. Para melhoria dos métodos de
+#'     estimação o parâmetro \eqn{\nu} é tomado como \eqn{\exp{\phi}} e
+#'     todas as inferências são tomadas a partir de \eqn{\phi}
+#' @param lambda Parâmetro \eqn{\lambda} da função de distribuição de
+#'     probabilidades COM-Poisson
+#' @param phi Parâmetro \eqn{\phi = \log{\nu}} da função de distribuição
+#'     de probabilidades COM-Poisson
+#' @param tol Critério de parada do algoritmo, representa o valor
+#'     tolerado para a diferença do valor de \eqn{Z(\lambda, \phi)}
+#'     entre duas iterações. O valor padrão é 1e-3
+#' @param maxit Número máximo de iterações a serem realizadas pelo
+#'     algoritmo. Se este número for atingido e o critério de tolerância
+#'     não for atendido, uma mensagem de aviso será exibida 
+#' @return O valor da constante de normalização, \eqn{Z(\lambda,
+#'     \phi)} da distribuição COM-Poisson
+#' @author Eduardo E. R. Junior, \email{edujrrib@gmail.com}
+#' @examples
+#'
+#' ## Verificação da função
+#' data(cottonBolls)
+#' formula <- ncap ~ est:(des + I(des^2))
+#' data <- cottonBolls
+#' 
+#' frame <- model.frame(formula, data)
+#' terms <- attr(frame, "terms")
+#' y <- model.response(frame)
+#' X <- model.matrix(terms, frame)
+#' 
+#' betas <- coef(glm(formula, data = data, family = poisson))
+#' phi <- 0
+#' Xb <- X %*% betas
+#' kernel <- sum(y * Xb - exp(phi) * lfactorial(y))
+#' 
+#' ## --- Utilizando a soma até 150
+#' i <- 0:150
+#' zs <- sapply(Xb, function(lam)
+#'     sum(exp(i * lam - exp(phi) * lfactorial(i))))
+#' (Z <- sum(log(zs)))
+#' (ll <- kernel - Z)
+#' 
+#' ## --- Utilizando a computez, com critério de parada
+#' zs1 <- sapply(exp(Xb), tccPackage:::computez, phi = phi)
+#' (Z1 <- sum(log(zs1)))
+#' (ll1 <- kernel - Z1)
+#'
+
+computez <- function(lambda, phi, tol = 1e-3, maxit = 1e3) {
+    nu <- exp(phi)
+    z0 <- 1000
+    z <- 0
+    i <- 2
+    while (abs(z - z0) > tol && i < maxit) {
+        z0 <- z
+        z <- z0 + exp(i * log(lambda) - nu * lfactorial(i))
+        i <- i + 1
+    }
+    if (abs(z - z0) > tol && i == maxit) {
+        diff <- abs(z - z0)
+        aviso <- sprintf(paste0(
+            "  Valor da constante de normaliza\\u00e7\\u00e3o",
+            " n\\u00e3o convergiu!\\n",
+            "    Estimada em %.3f com %i itera\\u00e7\\u00f5es\\n",
+            "    Diferen\\u00e7a entre 2 \\u00faltimas",
+            " itera\\u00e7\\u00f5es %.5f"),
+            z, i, diff)
+        warning(aviso)
+    }
+    return(z + lambda + 1)
+}
+
 #' @title Log-Verossimilhança do Modelo Conway-Maxwell-Poisson
 #' @description Calcula a log-verossimilhança de um modelo COM-Poisson
 #'     considerando os dados e as estimativas dos parâmetros informadas.
@@ -28,6 +101,12 @@ llcmp <- function(betas, phi, y, X, offset = NULL){
     zs <- sapply(Xb, function(lam)
         sum(exp(i * lam - nu * lfactorial(i))))
     Z <- sum(log(zs + 1))
+    ##-------------------------------------------
+    ## Ainda não funciona bem, o algoritmo de otimização leva a valores
+    ## absurdos de phi, inviabilizando o cálculo de Z
+    ## zs <- sapply(exp(Xb), computez, phi = phi)
+    ## Z <- sum(log(zs))
+    ##-------------------------------------------
     ll <- kernel - Z
     return(ll)
 }
