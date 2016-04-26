afb59d71a990e681d493931bbeca757ecb831e9a
Eduardo Junior <edujrrib@gmail.com>
2016-04-25 23:13:56 -0300

    Adiciona dados fish como exemplo para excesso de zeros

--- R/datasets.R
+++ R/datasets.R
@@ -386,3 +386,45 @@ NULL
 #'        jitter.x = TRUE,
 #'        type = c("p", "g", "smooth"))
 NULL
+
+#' @name fish
+#' @title Peixes Capturados por Pescadores em um Parque Estadual
+#' @description Dados sobre 250 grupos que foram ao parque ao parque
+#'     estadual para pescar. As informações coletadas foram refentes a
+#'     presenção ou não de um campista, ao número de crianças no grupo e
+#'     ao número de indivíduos no grupo.
+#' @format Um \code{data.frame} com 250 observações e 4 variáveis.
+#'     \describe{
+#' 
+#' \item{\code{campista}}{Fator com dois níveis que representa a
+#'     presença (\code{1}) ou ausência (\code{0}) de um campista no
+#'     grupo.}
+#' 
+#' \item{\code{ncriancas}}{Número de crianças no grupo.}
+#' 
+#' \item{\code{npessoas}}{Número total de pessoas no grupo.}
+#' 
+#' \item{\code{npeixes}}{Número de peixes capturados pelo grupo.}
+#' 
+#' }
+#' @keywords excesso-zeros
+#' @references Calvin, J. A. (1998). Regression Models for Categorical
+#'     and Limited Dependent Variables. Technometrics, 40(1), 80-81.
+#' @examples
+#'
+#' data(fish)
+#' (proptb <- prop.table(table(fish$npeixes)))
+#' plot(proptb)
+#' 
+#' library(lattice)
+#' # Contagens (marginal aos efeitos das covariáveis)
+#' histogram(~npeixes, data = fish, nint = 50)
+#' 
+#' # Contagens com relação as covariáveis
+#' xyplot(npeixes ~ ncriancas + npessoas,
+#'        groups = campista,
+#'        data = fish,
+#'        jitter.x = TRUE,
+#'        type = c("p", "g", "smooth"))
+#'
+NULL
